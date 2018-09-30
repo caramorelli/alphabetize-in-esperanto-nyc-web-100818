adb7dba3e62cb07555ce950962465a5c50690e9a
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:51:36 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,6 @@ def alphabetize(arr)
   new_arr = []
   esperanto_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |str|
-    puts str
     str.chars.each { |char| esperanto_alpha.index(char)}
   end 
 end
