378de88abea090911d2f9ddfe7dc56daeca03cb8
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:48:49 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
 def alphabetize(arr)
-  # code here
+  new_arr = []
+  esperanto_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_alpha.chars.each do |char|
+    arr.each do |word|
+      word.each_with_index do |letter|
+        if letter == char && 
+          new_arr << word
+        else 
+          
+      end 
+    end 
+  end
+  
+  print new_arr
+  puts
 end
+
+
+
+alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
+alphabetize(["ŝĝĉĥŭĵ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĉ"])  #["ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĵ"]
