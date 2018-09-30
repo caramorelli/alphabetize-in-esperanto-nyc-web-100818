14e8aa5e54db245297a177910794daf095912d5a
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:50:08 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,7 @@ def alphabetize(arr)
 
   esperanto_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |str|
+    str.chars.map { |char| esperanto_alpha.index(char)}
   end 
   
 end
@@ -11,3 +12,10 @@ end
 alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
 alphabetize(["ŝĝĉĥŭĵ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĉ"])  #["ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĵ"]
 
+def alphabetize(sentences_array)
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
+end
